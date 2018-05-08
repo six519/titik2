af2057f752d2bae4b09a0e0a9c8dcdc51a03ce1b
Ferdinand Silva <ferdinandsilva@ferdinandsilva.com>
2018-05-08 13:59:35 +0800

    print version

--- info/info.go
+++ info/info.go
@@ -2,7 +2,12 @@ package info
 
 import (
 	"fmt"
+	"runtime"
 )
+
+const TITIK_APP_NAME string = "Titik"
+const TITIK_STRING_VERSION string = "2.0.0"
+const TITIK_AUTHOR string = "Ferdinand E. Silva"
 
 func Help(exeName string) {
 	fmt.Printf("Usage: %s [-options] filename.ttk\n", exeName)
@@ -10,4 +15,10 @@ func Help(exeName string) {
 	fmt.Printf("\t-v\tget current version\n")
 	fmt.Printf("\t-i\topen interactive shell\n")
 	fmt.Printf("\t-h\tprint this usage info\n")
+}
+
+func Version() {
+    fmt.Printf("%s %s\n", TITIK_APP_NAME, TITIK_STRING_VERSION);
+	fmt.Printf("By: %s\n", TITIK_AUTHOR);
+	fmt.Printf("Operating System: %s\n", runtime.GOOS);
 }
--- main.go
+++ main.go
@@ -10,4 +10,13 @@ func main() {
 		info.Help(os.Args[0])
 		os.Exit(1)
 	}
+
+	if(os.Args[1] == "-v") {
+		info.Version()
+	} else if (os.Args[1] == "-h") {
+		info.Help(os.Args[0])
+	} else if (os.Args[1] == "-i") {
+	} else {
+		//open titik file
+	}
 }
