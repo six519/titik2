d9b6c5e80b9912bf3062952b8a5bf57901c242a6
Ferdinand Silva <ferdinandsilva@ferdinandsilva.com>
2018-05-09 12:37:05 +0800

    fix bug

--- lexer/lexer.go
+++ lexer/lexer.go
@@ -4,6 +4,7 @@ import (
 	"os"
 	"bufio"
 	"errors"
+	//"fmt"
 )
 
 type Lexer struct {
@@ -11,7 +12,7 @@ type Lexer struct {
 	fileContents []string
 }
 
-func (lexer Lexer) ReadSourceFile() error {
+func (lexer *Lexer) ReadSourceFile() error {
 
 	file, err := os.Open(lexer.FileName)
 	defer file.Close()
@@ -33,6 +34,14 @@ func (lexer Lexer) ReadSourceFile() error {
 
 func (lexer Lexer) GenerateToken() ([]map[string]string, error) {
 	var tokenArray []map[string]string
+
+	//read the file contents line by line
+	for x := 0; x < len(lexer.fileContents); x++ {
+		/*
+		fmt.Println(lexer.fileContents[x])
+		fmt.Printf("Line #%d \n", x + 1)
+		*/
+	}
 
 	return tokenArray, nil
 }
