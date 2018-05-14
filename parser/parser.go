57db694586fe014a79da99b2a2a9459141780d5f
Ferdinand Silva <ferdinandsilva@ferdinandsilva.com>
2018-05-14 18:23:36 +0800

    cleanup token

--- parser/parser.go
+++ parser/parser.go
@@ -8,6 +8,16 @@ type Parser struct {
 }
 
 func (parser Parser) Parse(tokenArray []lexer.Token) error {
+	var strippedTokenArray []lexer.Token
+
+	//remove spaces, newlines, comments and tabs
+	for x := 0; x < len(tokenArray); x++ {
+		if(tokenArray[x].Type != lexer.TOKEN_TYPE_NEWLINE && tokenArray[x].Type != lexer.TOKEN_TYPE_SPACE && tokenArray[x].Type != lexer.TOKEN_TYPE_SINGLE_COMMENT && tokenArray[x].Type != lexer.TOKEN_TYPE_MULTI_COMMENT && tokenArray[x].Type != lexer.TOKEN_TYPE_CLOSE_MULTI_COMMENT && tokenArray[x].Type != lexer.TOKEN_TYPE_CLOSE_STRING && tokenArray[x].Type != lexer.TOKEN_TYPE_TAB) {
+			strippedTokenArray = append(strippedTokenArray, tokenArray[x])
+		}
+	}
+
+	//parse the stripped tokens below
 
 	return nil
 }
