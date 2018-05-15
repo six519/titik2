04b6bf44ce7c5b39fac582c7f79b6babaf023e8a
Ferdinand Silva <ferdinandsilva@ferdinandsilva.com>
2018-05-15 13:34:04 +0800

    update parser

--- parser/parser.go
+++ parser/parser.go
@@ -4,11 +4,15 @@ import (
 	"github.com/six519/titik2/lexer"
 )
 
+//parser states
+const PARSER_STATE_START int = 0
+
 type Parser struct {
 }
 
 func (parser Parser) Parse(tokenArray []lexer.Token) error {
 	var strippedTokenArray []lexer.Token
+	parserState := PARSER_STATE_START
 
 	//remove spaces, newlines, comments and tabs
 	for x := 0; x < len(tokenArray); x++ {
@@ -18,6 +22,14 @@ func (parser Parser) Parse(tokenArray []lexer.Token) error {
 	}
 
 	//parse the stripped tokens below
+	for x := 0; x < len(strippedTokenArray); x++ {
+		switch(parserState) {
+			case PARSER_STATE_START:
+				//start
+			default:
+				continue
+		}
+	}
 
 	return nil
 }
