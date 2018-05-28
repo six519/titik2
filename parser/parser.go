a7ffc691dadd20752590554e957c9bc8f27de258
Ferdinand Silva <ferdinandsilva@ferdinandsilva.com>
2018-05-28 16:26:52 +0800

    add note

--- parser/parser.go
+++ parser/parser.go
@@ -1,21 +1,15 @@
 package parser
 
 import (
-	//"errors"
 	"github.com/six519/titik2/lexer"
-	//"github.com/six519/titik2/info"
 	"github.com/six519/titik2/variable"
 )
-
-//parser states
-//const PARSER_STATE_START int = 0
 
 type Parser struct {
 }
 
 func (parser Parser) Parse(tokenArray []lexer.Token, globalVariableArray *[]variable.Variable) error {
 	var strippedTokenArray []lexer.Token
-	//parserState := PARSER_STATE_START
 
 	//remove spaces, newlines, comments and tabs
 	for x := 0; x < len(tokenArray); x++ {
@@ -26,25 +20,7 @@ func (parser Parser) Parse(tokenArray []lexer.Token, globalVariableArray *[]vari
 
 	//parse the stripped tokens below
 	for x := 0; x < len(strippedTokenArray); x++ {
-		/*
-		switch(parserState) {
-			case PARSER_STATE_START:
-				//only accepts identifier & keywords at start
-				switch(strippedTokenArray[x].Type) {
-					case lexer.TOKEN_TYPE_IDENTIFIER:
-						//identifier type
-						//either assigment operation or user defined function call
-					case lexer.TOKEN_TYPE_KEYWORD:
-						//keyword type
-						//
-					default:
-						//token error
-						return errors.New(info.SyntaxErrorMessage(strippedTokenArray[x].Line, strippedTokenArray[x].Column, "Unexpected token", strippedTokenArray[x].FileName))
-				}
-			default:
-				continue
-		}
-		*/
+		//TRY TO APPLY SHUNTING-YARD HERE :TODO
 	}
 
 	return nil
