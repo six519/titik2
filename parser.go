a6eb4d245391886d06cd37cadc8340c878f1cce6
Ferdinand Silva <ferdinandsilva@ferdinandsilva.com>
2018-06-02 08:33:31 +0800

    update parser

--- parser.go
+++ parser.go
@@ -1,14 +1,27 @@
 package main
 
+import (
+	"errors"
+)
+
 type Parser struct {
 }
 
 func (parser Parser) Parse(tokenArray []Token, globalVariableArray *[]Variable) error {
 
+	operatorPrecedences := map[string] int{"+": 0, "-": 0, "/": 1, "*": 1} //operator order of precedences
+	var operatorStack []Token
+	var outputQueue []Token
+
 	for x := 0; x < len(tokenArray); x++ {
 		//ignore newline, space, tab and comments
 		if(tokenArray[x].Type != TOKEN_TYPE_NEWLINE && tokenArray[x].Type != TOKEN_TYPE_SPACE && tokenArray[x].Type != TOKEN_TYPE_SINGLE_COMMENT && tokenArray[x].Type != TOKEN_TYPE_MULTI_COMMENT && tokenArray[x].Type != TOKEN_TYPE_CLOSE_MULTI_COMMENT && tokenArray[x].Type != TOKEN_TYPE_CLOSE_STRING && tokenArray[x].Type != TOKEN_TYPE_TAB) {
-			//TODO: TRY TO APPLY SHUNTING-YARD BELOW
+
+			if(tokenArray[x].Type == TOKEN_TYPE_PLUS || tokenArray[x].Type == TOKEN_TYPE_MINUS || tokenArray[x].Type == TOKEN_TYPE_DIVIDE || tokenArray[x].Type == TOKEN_TYPE_MULTIPLY) {
+				//syntax error if the first token is an operator
+				return errors.New(SyntaxErrorMessage(tokenArray[x].Line, tokenArray[x].Column, "Unexpected token '" + tokenArray[x].Value + "'", tokenArray[x].FileName))
+			}
+
 		}
 	}
 
