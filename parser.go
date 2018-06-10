378d6fd74698f74ca0490b696b90494252a9130f
Ferdinand Silva <ferdinandsilva@ferdinandsilva.com>
2018-06-10 19:01:08 +0800

    fix bug

--- parser.go
+++ parser.go
@@ -14,7 +14,7 @@ func (parser Parser) Parse(tokenArray []Token, globalVariableArray *[]Variable, 
 	for x := 0; x < len(tokenArray); x++ {
 		//ignore space, tab and comments
 		if(tokenArray[x].Type != TOKEN_TYPE_SPACE && tokenArray[x].Type != TOKEN_TYPE_SINGLE_COMMENT && tokenArray[x].Type != TOKEN_TYPE_MULTI_COMMENT && tokenArray[x].Type != TOKEN_TYPE_CLOSE_MULTI_COMMENT && tokenArray[x].Type != TOKEN_TYPE_CLOSE_STRING && tokenArray[x].Type != TOKEN_TYPE_TAB) {
-			if(tokenArray[x].Type != TOKEN_TYPE_NEWLINE) {
+			if(tokenArray[x].Type == TOKEN_TYPE_NEWLINE) {
 				//execute shunting yard
 			} else {
 				//put the token to stack for shunting yard process later
