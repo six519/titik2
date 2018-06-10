3c3c42ac7063ebfb66d5351c8bfa512d470965aa
Ferdinand Silva <ferdinandsilva@ferdinandsilva.com>
2018-06-10 15:08:18 +0800

    dump variable

--- main.go
+++ main.go
@@ -45,5 +45,6 @@ func main() {
 		}
 
 		//DumpToken(tokenArray) //:TEMPORARY
+		DumpVariable(globalVariableArray)
 	}
 }
--- variable.go
+++ variable.go
@@ -1,11 +1,23 @@
 package main
 
+import (
+	"fmt"
+)
+
 //variable types (basic)
 const VARIABLE_TYPE_NONE int = 0
 const VARIABLE_TYPE_INTEGER int = 1
-const VARIABLE_TYPE_STRING int = 3
-const VARIABLE_TYPE_FLOAT int = 4
-const VARIABLE_TYPE_ARRAY int = 0
+const VARIABLE_TYPE_STRING int = 2
+const VARIABLE_TYPE_FLOAT int = 3
+const VARIABLE_TYPE_ARRAY int = 4
+
+var VARIABLE_TYPES_STRING = []string {
+	"VARIABLE_TYPE_NONE",
+	"VARIABLE_TYPE_INTEGER",
+	"VARIABLE_TYPE_STRING",
+	"VARIABLE_TYPE_FLOAT",
+	"VARIABLE_TYPE_ARRAY",
+}
 
 type Variable struct {
 	Name string
@@ -16,6 +28,25 @@ type Variable struct {
 	FloatValue float64
 	IsConstant bool
 	ArrayValue []Variable
+}
+
+func DumpVariable(variables []Variable) {
+	fmt.Printf("====================================\n")
+
+	for x := 0; x < len(variables); x++ {
+		fmt.Printf("Variable Name: %s\n", variables[x].Name)
+
+		//NOTE: ASSUME VARIABLE TYPE AS FLOAT AND INTEGER FOR NOW (TEMPORARY)
+		if(variables[x].Type == VARIABLE_TYPE_FLOAT) {
+			fmt.Printf("Variable Value: %f\n", variables[x].FloatValue)
+		} else {
+			fmt.Printf("Variable Value: %d\n", variables[x].IntegerValue)
+		}
+
+		fmt.Printf("Variable Scope: %s\n", variables[x].ScopeName)
+        fmt.Printf("Variable Type: %s\n", VARIABLE_TYPES_STRING[variables[x].Type])
+		fmt.Printf("====================================\n")
+	}
 }
 
 func isVariableExists(token Token, globalVariableArray []Variable, scopeName string) (bool, int) {
