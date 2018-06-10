d3d9351773c8122914b4c070fced6bc106383fe9
Ferdinand Silva <ferdinandsilva@ferdinandsilva.com>
2018-06-10 19:29:21 +0800

    change var name

--- parser.go
+++ parser.go
@@ -10,7 +10,7 @@ type Parser struct {
 }
 
 func (parser Parser) Parse(tokenArray []Token, globalVariableArray *[]Variable, scopeName string) error {
-	var currentTokens []Token
+	var tokensToEvaluate []Token
 	for x := 0; x < len(tokenArray); x++ {
 		//ignore space, tab and comments
 		if(tokenArray[x].Type != TOKEN_TYPE_SPACE && tokenArray[x].Type != TOKEN_TYPE_SINGLE_COMMENT && tokenArray[x].Type != TOKEN_TYPE_MULTI_COMMENT && tokenArray[x].Type != TOKEN_TYPE_CLOSE_MULTI_COMMENT && tokenArray[x].Type != TOKEN_TYPE_CLOSE_STRING && tokenArray[x].Type != TOKEN_TYPE_TAB) {
@@ -18,7 +18,7 @@ func (parser Parser) Parse(tokenArray []Token, globalVariableArray *[]Variable, 
 				//execute shunting yard
 			} else {
 				//put the token to stack for shunting yard process later
-				currentTokens = append(currentTokens, tokenArray[x])
+				tokensToEvaluate = append(tokensToEvaluate, tokenArray[x])
 			}
 		}
 	}
