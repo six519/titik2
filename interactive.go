0c0c837424700b89258cc6fc2aec2f61db9c82ce
Ferdinand Silva <ferdinandsilva@ferdinandsilva.com>
2018-07-11 08:31:58 +0800

    update interactive shell

--- interactive.go
+++ interactive.go
@@ -9,6 +9,8 @@ import (
 func InteractiveShell(globalVariableArray *[]Variable, globalFunctionArray *[]Function, globalNativeVarList *[]string) {
 	var indicator string
 	var isContinue bool = false
+	var stringContainer string
+	var fdCount int = 0
 
 	fmt.Printf("%s %s\n", TITIK_APP_NAME, TITIK_STRING_VERSION)
 	fmt.Println("To exit, press ^C")
@@ -24,18 +26,39 @@ func InteractiveShell(globalVariableArray *[]Variable, globalFunctionArray *[]Fu
 		fmt.Printf("%s ", indicator)
 		text, _ := reader.ReadString('\n')
 		lxr := Lexer{FileName: "interactive_shell"}
-		lxr.ReadString(text)
+		stringContainer += text
+		lxr.ReadString(stringContainer)
 
 		tokenArray, tokenErr := lxr.GenerateToken()
+		fdCount = 0
 
 		if (tokenErr != nil) {
 			fmt.Println(tokenErr)
 		} else {
-			prsr := Parser{}
-			parserErr := prsr.Parse(tokenArray, globalVariableArray, globalFunctionArray, "main", globalNativeVarList)
-	
-			if(parserErr != nil) {
-				fmt.Println(parserErr)
+			for x := 0; x < len(tokenArray); x++ {
+				if(tokenArray[x].Type == TOKEN_TYPE_FUNCTION_DEF_START) {
+					fdCount += 1
+				}
+				if(tokenArray[x].Type == TOKEN_TYPE_FUNCTION_DEF_END) {
+					fdCount -= 1
+				}
+			}
+
+			if(fdCount > 0) {
+				isContinue = true
+			} else {
+				isContinue = false
+			}
+
+			if(!isContinue) {
+				prsr := Parser{}
+				parserErr := prsr.Parse(tokenArray, globalVariableArray, globalFunctionArray, "main", globalNativeVarList)
+		
+				if(parserErr != nil) {
+					fmt.Println(parserErr)
+				}
+
+				stringContainer = ""
 			}
 		}
 	}
