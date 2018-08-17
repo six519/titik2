c023d772d08fe8c1ca84eeabfa9d492f5e312fe4
Ferdinand Silva <ferdinandsilva@ferdinandsilva.com>
2018-08-17 20:16:02 +0800

    organize functions again

---
+++ conversion.go
@@ -0,0 +1,41 @@
+package main
+
+import (
+	"strconv"
+)
+
+func Toi_execute(arguments []FunctionArgument, errMessage *error) FunctionReturn {
+	ret := FunctionReturn{Type: RET_TYPE_INTEGER, IntegerValue: 0}
+
+	if(arguments[0].Type == ARG_TYPE_FLOAT) {
+		ret.IntegerValue = int(arguments[0].FloatValue) + 0
+	} else if(arguments[0].Type == ARG_TYPE_STRING) {
+		ret.IntegerValue, _ = strconv.Atoi(arguments[0].StringValue)
+	} else if(arguments[0].Type == ARG_TYPE_INTEGER) {
+		ret.IntegerValue = arguments[0].IntegerValue
+	}
+
+	return ret
+}
+
+func Tos_execute(arguments []FunctionArgument, errMessage *error) FunctionReturn {
+	ret := FunctionReturn{Type: RET_TYPE_STRING, StringValue: ""}
+
+	if(arguments[0].Type == ARG_TYPE_FLOAT) {
+		ret.StringValue = strconv.FormatFloat(arguments[0].FloatValue, 'f', -1, 64)
+	} else if(arguments[0].Type == ARG_TYPE_STRING) {
+		ret.StringValue = arguments[0].StringValue
+	} else if(arguments[0].Type == ARG_TYPE_INTEGER) {
+		ret.StringValue = strconv.Itoa(arguments[0].IntegerValue)
+	} else if(arguments[0].Type == ARG_TYPE_BOOLEAN) {
+		if(arguments[0].BooleanValue) {
+			ret.StringValue = "true"
+		} else {
+			ret.StringValue = "false"
+		}	
+	} else {
+		ret.StringValue = ""
+	}
+
+	return ret
+}
--- function.go
+++ function.go
@@ -2,7 +2,6 @@ package main
 
 import (
 	"fmt"
-	"strconv"
 	"errors"
 )
 
@@ -108,42 +107,6 @@ func ReverseBoolean_execute(arguments []FunctionArgument, errMessage *error) Fun
 		*errMessage = errors.New("Error: Parameter must be a boolean type")
 	} else {
 		ret.BooleanValue = !arguments[0].BooleanValue
-	}
-
-	return ret
-}
-
-func Toi_execute(arguments []FunctionArgument, errMessage *error) FunctionReturn {
-	ret := FunctionReturn{Type: RET_TYPE_INTEGER, IntegerValue: 0}
-
-	if(arguments[0].Type == ARG_TYPE_FLOAT) {
-		ret.IntegerValue = int(arguments[0].FloatValue) + 0
-	} else if(arguments[0].Type == ARG_TYPE_STRING) {
-		ret.IntegerValue, _ = strconv.Atoi(arguments[0].StringValue)
-	} else if(arguments[0].Type == ARG_TYPE_INTEGER) {
-		ret.IntegerValue = arguments[0].IntegerValue
-	}
-
-	return ret
-}
-
-func Tos_execute(arguments []FunctionArgument, errMessage *error) FunctionReturn {
-	ret := FunctionReturn{Type: RET_TYPE_STRING, StringValue: ""}
-
-	if(arguments[0].Type == ARG_TYPE_FLOAT) {
-		ret.StringValue = strconv.FormatFloat(arguments[0].FloatValue, 'f', -1, 64)
-	} else if(arguments[0].Type == ARG_TYPE_STRING) {
-		ret.StringValue = arguments[0].StringValue
-	} else if(arguments[0].Type == ARG_TYPE_INTEGER) {
-		ret.StringValue = strconv.Itoa(arguments[0].IntegerValue)
-	} else if(arguments[0].Type == ARG_TYPE_BOOLEAN) {
-		if(arguments[0].BooleanValue) {
-			ret.StringValue = "true"
-		} else {
-			ret.StringValue = "false"
-		}	
-	} else {
-		ret.StringValue = ""
 	}
 
 	return ret
