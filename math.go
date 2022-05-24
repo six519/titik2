0ec686672627b6a7140c273601a0bec07128129c
Ferdinand Silva <ferdinandsilva@ferdinandsilva.com>
2022-05-24 08:29:32 +0800

    refactor math functionalities

--- math.go
+++ math.go
@@ -1,31 +1,24 @@
 package main
+
 import (
+	"errors"
 	"math"
 	"strconv"
-	"errors"
 )
 
 func Abs_execute(arguments []FunctionArgument, errMessage *error, globalVariableArray *[]Variable, globalFunctionArray *[]Function, scopeName string, globalNativeVarList *[]string, globalSettings *GlobalSettingsObject, line_number int, column_number int, file_name string) FunctionReturn {
 	ret := FunctionReturn{Type: RET_TYPE_FLOAT, FloatValue: 0}
 
-	if(arguments[0].Type != ARG_TYPE_FLOAT && arguments[0].Type != ARG_TYPE_INTEGER) {
+	if arguments[0].Type != ARG_TYPE_FLOAT && arguments[0].Type != ARG_TYPE_INTEGER {
 		*errMessage = errors.New("Error: Parameter must be a float or integer type on line number " + strconv.Itoa(line_number) + " and column number " + strconv.Itoa(column_number) + ", Filename: " + file_name)
 	} else {
 		var output float64
 
-		if(arguments[0].Type == ARG_TYPE_FLOAT) {
+		if arguments[0].Type == ARG_TYPE_FLOAT {
 			output = math.Abs(arguments[0].FloatValue)
 		} else {
 			output = math.Abs(float64(arguments[0].IntegerValue))
 		}
-
-		/*
-		if(math.IsNaN(output)) {
-			ret.Type = RET_TYPE_NONE
-		} else {
-			ret.FloatValue = output
-		}
-		*/
 
 		ret.FloatValue = output
 
@@ -37,18 +30,18 @@ func Abs_execute(arguments []FunctionArgument, errMessage *error, globalVariable
 func Acs_execute(arguments []FunctionArgument, errMessage *error, globalVariableArray *[]Variable, globalFunctionArray *[]Function, scopeName string, globalNativeVarList *[]string, globalSettings *GlobalSettingsObject, line_number int, column_number int, file_name string) FunctionReturn {
 	ret := FunctionReturn{Type: RET_TYPE_FLOAT, FloatValue: 0}
 
-	if(arguments[0].Type != ARG_TYPE_FLOAT && arguments[0].Type != ARG_TYPE_INTEGER) {
+	if arguments[0].Type != ARG_TYPE_FLOAT && arguments[0].Type != ARG_TYPE_INTEGER {
 		*errMessage = errors.New("Error: Parameter must be a float or integer type on line number " + strconv.Itoa(line_number) + " and column number " + strconv.Itoa(column_number) + ", Filename: " + file_name)
 	} else {
 		var output float64
 
-		if(arguments[0].Type == ARG_TYPE_FLOAT) {
+		if arguments[0].Type == ARG_TYPE_FLOAT {
 			output = math.Acos(arguments[0].FloatValue)
 		} else {
 			output = math.Acos(float64(arguments[0].IntegerValue))
 		}
 
-		if(math.IsNaN(output)) {
+		if math.IsNaN(output) {
 			ret.Type = RET_TYPE_NONE
 		} else {
 			ret.FloatValue = output
@@ -64,18 +57,18 @@ func Acs_execute(arguments []FunctionArgument, errMessage *error, globalVariable
 func Acsh_execute(arguments []FunctionArgument, errMessage *error, globalVariableArray *[]Variable, globalFunctionArray *[]Function, scopeName string, globalNativeVarList *[]string, globalSettings *GlobalSettingsObject, line_number int, column_number int, file_name string) FunctionReturn {
 	ret := FunctionReturn{Type: RET_TYPE_FLOAT, FloatValue: 0}
 
-	if(arguments[0].Type != ARG_TYPE_FLOAT && arguments[0].Type != ARG_TYPE_INTEGER) {
+	if arguments[0].Type != ARG_TYPE_FLOAT && arguments[0].Type != ARG_TYPE_INTEGER {
 		*errMessage = errors.New("Error: Parameter must be a float or integer type on line number " + strconv.Itoa(line_number) + " and column number " + strconv.Itoa(column_number) + ", Filename: " + file_name)
 	} else {
 		var output float64
 
-		if(arguments[0].Type == ARG_TYPE_FLOAT) {
+		if arguments[0].Type == ARG_TYPE_FLOAT {
 			output = math.Acosh(arguments[0].FloatValue)
 		} else {
 			output = math.Acosh(float64(arguments[0].IntegerValue))
 		}
 
-		if(math.IsNaN(output)) {
+		if math.IsNaN(output) {
 			ret.Type = RET_TYPE_NONE
 		} else {
 			ret.FloatValue = output
@@ -91,18 +84,18 @@ func Acsh_execute(arguments []FunctionArgument, errMessage *error, globalVariabl
 func Asn_execute(arguments []FunctionArgument, errMessage *error, globalVariableArray *[]Variable, globalFunctionArray *[]Function, scopeName string, globalNativeVarList *[]string, globalSettings *GlobalSettingsObject, line_number int, column_number int, file_name string) FunctionReturn {
 	ret := FunctionReturn{Type: RET_TYPE_FLOAT, FloatValue: 0}
 
-	if(arguments[0].Type != ARG_TYPE_FLOAT && arguments[0].Type != ARG_TYPE_INTEGER) {
+	if arguments[0].Type != ARG_TYPE_FLOAT && arguments[0].Type != ARG_TYPE_INTEGER {
 		*errMessage = errors.New("Error: Parameter must be a float or integer type on line number " + strconv.Itoa(line_number) + " and column number " + strconv.Itoa(column_number) + ", Filename: " + file_name)
 	} else {
 		var output float64
 
-		if(arguments[0].Type == ARG_TYPE_FLOAT) {
+		if arguments[0].Type == ARG_TYPE_FLOAT {
 			output = math.Asin(arguments[0].FloatValue)
 		} else {
 			output = math.Asin(float64(arguments[0].IntegerValue))
 		}
 
-		if(math.IsNaN(output)) {
+		if math.IsNaN(output) {
 			ret.Type = RET_TYPE_NONE
 		} else {
 			ret.FloatValue = output
@@ -118,18 +111,18 @@ func Asn_execute(arguments []FunctionArgument, errMessage *error, globalVariable
 func Asnh_execute(arguments []FunctionArgument, errMessage *error, globalVariableArray *[]Variable, globalFunctionArray *[]Function, scopeName string, globalNativeVarList *[]string, globalSettings *GlobalSettingsObject, line_number int, column_number int, file_name string) FunctionReturn {
 	ret := FunctionReturn{Type: RET_TYPE_FLOAT, FloatValue: 0}
 
-	if(arguments[0].Type != ARG_TYPE_FLOAT && arguments[0].Type != ARG_TYPE_INTEGER) {
+	if arguments[0].Type != ARG_TYPE_FLOAT && arguments[0].Type != ARG_TYPE_INTEGER {
 		*errMessage = errors.New("Error: Parameter must be a float or integer type on line number " + strconv.Itoa(line_number) + " and column number " + strconv.Itoa(column_number) + ", Filename: " + file_name)
 	} else {
 		var output float64
 
-		if(arguments[0].Type == ARG_TYPE_FLOAT) {
+		if arguments[0].Type == ARG_TYPE_FLOAT {
 			output = math.Asinh(arguments[0].FloatValue)
 		} else {
 			output = math.Asinh(float64(arguments[0].IntegerValue))
 		}
 
-		if(math.IsNaN(output)) {
+		if math.IsNaN(output) {
 			ret.Type = RET_TYPE_NONE
 		} else {
 			ret.FloatValue = output
@@ -145,18 +138,18 @@ func Asnh_execute(arguments []FunctionArgument, errMessage *error, globalVariabl
 func Atn_execute(arguments []FunctionArgument, errMessage *error, globalVariableArray *[]Variable, globalFunctionArray *[]Function, scopeName string, globalNativeVarList *[]string, globalSettings *GlobalSettingsObject, line_number int, column_number int, file_name string) FunctionReturn {
 	ret := FunctionReturn{Type: RET_TYPE_FLOAT, FloatValue: 0}
 
-	if(arguments[0].Type != ARG_TYPE_FLOAT && arguments[0].Type != ARG_TYPE_INTEGER) {
+	if arguments[0].Type != ARG_TYPE_FLOAT && arguments[0].Type != ARG_TYPE_INTEGER {
 		*errMessage = errors.New("Error: Parameter must be a float or integer type on line number " + strconv.Itoa(line_number) + " and column number " + strconv.Itoa(column_number) + ", Filename: " + file_name)
 	} else {
 		var output float64
 
-		if(arguments[0].Type == ARG_TYPE_FLOAT) {
+		if arguments[0].Type == ARG_TYPE_FLOAT {
 			output = math.Atan(arguments[0].FloatValue)
 		} else {
 			output = math.Atan(float64(arguments[0].IntegerValue))
 		}
 
-		if(math.IsNaN(output)) {
+		if math.IsNaN(output) {
 			ret.Type = RET_TYPE_NONE
 		} else {
 			ret.FloatValue = output
